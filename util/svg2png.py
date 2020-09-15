916116d5caa260d09600741a15024d0f9606c565
chaofeng <luke-feng@outlook.com>
2020-09-15 17:27:50 +0200

    Update svg2png.py

--- util/svg2png.py
+++ util/svg2png.py
@@ -7,5 +7,3 @@ svg_path = path+"21527.svg"
 png_path = path + "file.png"
 
 cairosvg.svg2png(url=svg_path,write_to=png_path)
-'''drawing = svg2rlg(svg_path)
-renderPM.drawToFile(drawing, png_path, fmt="PNG")'''
